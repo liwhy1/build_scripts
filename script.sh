aa2c2eddcbd2044380be74762b42cd9c3912ca33
Liwhy <100075328+liwhy1@users.noreply.github.com>
2024-10-30 21:59:40 +0100

    Update script

--- script.sh
+++ script.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-rm -rf device/oplus/denniz
+# Clear folders
+rm -rf .repo/local_manifests/
+rm -rf device/oplus/denniz/
 
-# repo init rom
+# Init ROM manifest
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
-# Local manifests
+# Clone local manifest
 git clone https://github.com/oplus-ossi-development/local_manifests --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Device tree
-git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-21_wip device/oplus/denniz
+# Clone additional trees
+git clone https://github.com/liwhy1/android_device_oplus_denniz --depth=1 -b lineage-21_wip device/oplus/denniz
 echo "============================"
-echo "Device tree clone success"
+echo "Additional trees clone success"
 echo "============================"
 
 # Sync
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
+# OPTIONAL
 # Use AOSP clang for no errors
 # rm -r prebuilts/clang/host/linux-x86
 # git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 prebuilts/clang/host/linux-x86
@@ -37,15 +38,15 @@ echo "============="
 # chmod a+x create-signed-env.sh
 # ./create-signed-env.sh
 
-# Export
+# Export parameters
 export BUILD_USERNAME=Liwhy
 export BUILD_HOSTNAME=crave
+export USE_CCACHE=1
+ccache -M 50G
 echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
-export USE_CCACHE=1
-ccache -M 50G
 lunch lineage_denniz-ap2a-userdebug
 echo "====== Envsetup Done ======="
 
