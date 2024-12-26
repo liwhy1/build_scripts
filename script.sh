4b1f3fe72c2cccb26ccbdb01a9a215d084bbd44b
Liwhy <100075328+liwhy1@users.noreply.github.com>
2024-12-26 17:31:05 +0100

    denniz: Bringup Lineage

--- script.sh
+++ script.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-# Sample script by Liwhy
 
 # Clear folders
-rm -rf .repo/local_manifests/
-rm -rf device/xiaomi/sapphire/
+rm -rf .repo/local_manifests
+rm -rf device/oplus/denniz
+rm -rf vendor/oplus/denniz
+rm -rf device/oplus/mt6893-common
+rm -rf vendor/oplus/mt6893-common
+rm -rf kernel/oplus/mt6893
 
 # Init ROM manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
 echo "=================="
 echo "ROM manifest init success"
 echo "=================="
 
 # Clone local manifest
-git clone https://github.com/Liekoo/local-manifest --depth=1 -b 14 .repo/local_manifests
+git clone https://github.com/liwhy1/local_manifests -b lineage-22 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Clone additional trees
-git clone https://github.com/Liekoo/device_xiaomi_sapphire --depth=1 -b main device/xiaomi/sapphire
-echo "============================"
-echo "Additional trees clone success"
-echo "============================"
+git submodule update --init --recursive
 
 # Sync
 /opt/crave/resync.sh
@@ -29,26 +28,15 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# OPTIONAL
-# Use AOSP clang for no errors
-# rm -r prebuilts/clang/host/linux-x86
-# git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 prebuilts/clang/host/linux-x86
-
-# Auto-sign build
-# wget https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh
-# chmod a+x create-signed-env.sh
-# ./create-signed-env.sh
-
 # Export parameters
-export BUILD_USERNAME=User
+export BUILD_USERNAME=Liwhy
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
 # Set up build environment
-source build/envsetup.sh
-lunch lineage_sapphire-ap2a-userdebug
+. build/envsetup.sh
+breakfast denniz
 echo "====== Envsetup Done ======="
 
-# Build ROM
-make installclean
-mka bacon
+# Build signed ROM
+bash build_signed.sh
