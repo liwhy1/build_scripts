800a05073ae06d9723ed794b30ec754210a5ff28
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-03 19:13:15 +0100

    denniz: Update script

--- script.sh
+++ script.sh
@@ -4,15 +4,20 @@
 rm -rf .repo/local_manifests
 rm -rf device/oplus/denniz
 rm -rf device/oplus/mt6893-common
+rm -rf vendor/oplus/denniz
+rm -rf vendor/oplus/mt6893-common
+echo "======================="
+echo "Old directories removed"
+echo "======================="
 
 # Init ROM manifest
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
-echo "=================="
+echo "========================="
 echo "ROM manifest init success"
-echo "=================="
+echo "========================="
 
 # Clone local manifest
-git clone https://github.com/liwhy1/local_manifests -b lineage-22 .repo/local_manifests
+git clone https://github.com/liwhy1/local_manifests -b lineage-22.1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -31,12 +36,12 @@ echo "Export Done"
 echo "============"
 
 # Set up build environment
-. build/envsetup.sh
-lunch lineage_denniz-ap3a-userdebug
+source build/envsetup.sh
 echo "============="
 echo "Envsetup Done"
 echo "============="
 
 # Build signed ROM
-breakfast denniz
-mka bacon
+breakfast denniz userdebug; \
+mka target-files-package otatools; \
+/opt/crave/crave_sign.sh
