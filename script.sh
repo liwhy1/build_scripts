99a90afd8f8c5453e2f822749779bb61287ff08a
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-20 22:26:34 +0100

    denniz: Add wip kernel source

--- script.sh
+++ script.sh
@@ -35,6 +35,7 @@ echo "============="
 rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
 git clone https://gitlab.com/nattolecats/android_device_oplus_camera -b lineage-22 device/oplus/camera
+git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
 
 # Set up build environment
 cd $cwd
