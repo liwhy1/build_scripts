82aacb4203af699cbd7a925e2af356451e806781
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-21 15:27:57 +0100

    denniz: Update camera source

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ echo "============="
 # Clone WIP trees
 rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
-git clone https://gitlab.com/nattolecats/android_device_oplus_camera -b lineage-22 device/oplus/camera
+git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22 device/oplus/camera
 
 # Set up build environment
 cd $cwd
