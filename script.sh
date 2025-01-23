ce1153f228331b88b8f484d78a343847cda50375
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-23 17:30:58 +0100

    denniz: Use new kernel with ksu next & susfs

--- script.sh
+++ script.sh
@@ -34,8 +34,10 @@ echo "============="
 # Clone WIP trees
 rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
+rm -rf kernel/oplus/mt6893-common/
+git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
 rm -rf vendor/oplus/denniz/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
+
 git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22 device/oplus/camera
 
 # Set up build environment
