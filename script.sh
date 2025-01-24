edd081d93b5b7fb0f76a669c86e2c40d50953187
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-24 10:14:49 +0100

    denniz: Update kernel revision

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ echo "============="
 
 # Clone wip trees
 rm -rf kernel/oplus/mt6893-common/
-git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
+git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893-common
 
 # Set up build environment
 cd $cwd
