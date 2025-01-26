1bc0694ac847892ad1ef425174769294500073b2
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-26 18:58:44 +0100

    denniz: Drop wip trees for now

--- script.sh
+++ script.sh
@@ -33,13 +33,13 @@ echo "Sync success"
 echo "============="
 
 # Clone WIP trees
-rm -rf device/oplus/mt6893-common/
-git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
-rm -rf kernel/oplus/mt6893-common/
-git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
-rm -rf vendor/oplus/denniz/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
-git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22 device/oplus/camera
+#rm -rf device/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
+#rm -rf kernel/oplus/mt6893-common/
+#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
+#rm -rf vendor/oplus/denniz/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
+#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22 device/oplus/camera
 
 # Set up build environment
 cd $cwd
