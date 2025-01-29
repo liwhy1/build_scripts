d45395217cbeaccedebe8a60956708be8767e9b5
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-29 15:32:36 +0100

    Cleanup

--- script.sh
+++ script.sh
@@ -7,49 +7,59 @@ rm -rf device/oplus/denniz/
 rm -rf device/oplus/mt6893-common/
 rm -rf vendor/oplus/denniz/
 rm -rf vendor/oplus/mt6893-common/
-rm -rf kernel/oplus/mt6893-common/
 rm -rf vendor/lineage-priv/keys/
+rm -rf kernel/oplus/mt6893/
+rm -rf device/oplus/camera/
 rm -rf prebuilts/clang/host/linux-x86/
-echo "======================="
-echo "Old directories removed"
-echo "======================="
+echo "============================="
+echo "Old directory remove finished"
+echo "============================="
 
 # Init ROM manifest
 #repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
-#echo "========================="
-#echo "ROM manifest init success"
-#echo "========================="
+#echo "=========================="
+#echo "ROM manifest init finished"
+#echo "=========================="
 
 # Clone local manifest
 git clone https://github.com/liwhy1/local_manifests -b rising-6 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+echo "============================="
+echo "Local manifest clone finished"
+echo "============================="
+
+# Clone signing keys
+git clone https://github.com/liwhy1/build_scripts -b rising_keys vendor/lineage-priv/keys
+echo "==========================="
+echo "Signing keys clone finished"
+echo "==========================="
 
 # Sync
 /opt/crave/resync.sh
 echo "============="
-echo "Sync success"
+echo "Sync finished"
 echo "============="
 
 # Clone WIP trees
 rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
 #rm -rf kernel/oplus/mt6893-common/
-#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kernelsu-next-susfs kernel/oplus/mt6893-common
+#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
 rm -rf vendor/oplus/denniz/
 git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
+rm -rf device/oplus/camera/
 git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22 device/oplus/camera
 
 # Set up build environment
 cd $cwd
 . build/envsetup.sh
-echo "============="
-echo "Envsetup Done"
-echo "============="
+echo "================="
+echo "Envsetup finished"
+echo "================="
 
 # Build signed
 cd $cwd
-git clone https://github.com/liwhy1/build_scripts -b rising_keys vendor/lineage-priv/keys
+echo "=============="
+echo "Starting build"
+echo "=============="
 riseup denniz userdebug
 rise sb
