712222d37cec64e77c2d94e357dab0c98e9e4c24
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-12 22:35:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,7 +41,7 @@ echo "============="
 
 # Clone WIP trees
 rm -rf device/oplus/mt6893-common/
-git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
+git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip device/oplus/mt6893-common
 #rm -rf kernel/oplus/mt6893-common/
 #git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
 rm -rf vendor/oplus/denniz/
