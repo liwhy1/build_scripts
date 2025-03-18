1b731de40ee7149ecc8a4bed820e24c6386d89bb
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-18 16:58:32 +0100

    Clone hw/oplus

--- script.sh
+++ script.sh
@@ -50,6 +50,8 @@ rm -rf vendor/oplus/mt6893-common/
 git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
 rm -rf device/oplus/camera/
 git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
+rm -rf hardware/oplus/
+git clone https://github.com/Evolution-X-Devices/hardware_oplus -b vic hardware/oplus
 
 # Set up build environment
 cd $cwd
