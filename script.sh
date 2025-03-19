43dc9b5caad1171fd2825415abb98343e257c416
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-19 19:20:17 +0100

    no sync 4 now

--- script.sh
+++ script.sh
@@ -2,27 +2,27 @@
 
 # Clear folders
 cwd=$(pwd)
-rm -rf .repo/local_manifests/
-rm -rf device/oplus/denniz/
-rm -rf device/oplus/mt6893-common/
-rm -rf vendor/oplus/denniz/
-rm -rf vendor/oplus/mt6893-common/
-rm -rf vendor/evolution-priv/keys/
-rm -rf kernel/oplus/mt6893/
-rm -rf device/oplus/camera/
-rm -rf prebuilts/clang/host/linux-x86/
+#rm -rf .repo/local_manifests/
+#rm -rf device/oplus/denniz/
+#rm -rf device/oplus/mt6893-common/
+#rm -rf vendor/oplus/denniz/
+#rm -rf vendor/oplus/mt6893-common/
+#rm -rf vendor/evolution-priv/keys/
+#rm -rf kernel/oplus/mt6893/
+#rm -rf device/oplus/camera/
+#rm -rf prebuilts/clang/host/linux-x86/
 echo "============================="
 echo "Old directory remove finished"
 echo "============================="
 
 # Init ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b vic-qpr1 --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b vic-qpr1 --git-lfs
 echo "=========================="
 echo "ROM manifest init finished"
 echo "=========================="
 
 # Clone local manifest
-git clone https://github.com/liwhy1/local_manifests -b evox-10 .repo/local_manifests
+#git clone https://github.com/liwhy1/local_manifests -b evox-10 .repo/local_manifests
 echo "============================="
 echo "Local manifest clone finished"
 echo "============================="
@@ -34,7 +34,7 @@ echo "Signing keys clone finished"
 echo "==========================="
 
 # Sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 echo "============="
 echo "Sync finished"
 echo "============="
