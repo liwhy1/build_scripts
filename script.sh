5678760d0c037e686e3ceb7c20eaad90e4a626b3
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-21 19:08:09 +0100

    Rework script to be more easy to edit

--- script.sh
+++ script.sh
@@ -1,58 +1,70 @@
 #!/bin/bash
 
-# Clear folders
-cwd=$(pwd)
-rm -rf .repo/local_manifests/
-rm -rf device/oplus/denniz/
-rm -rf device/oplus/mt6893-common/
-rm -rf vendor/oplus/denniz/
-rm -rf vendor/oplus/mt6893-common/
-rm -rf vendor/evolution-priv/keys/
-rm -rf kernel/oplus/mt6893/
-rm -rf device/oplus/camera/
-rm -rf hardware/oplus
-rm -rf prebuilts/clang/host/linux-x86/
-echo "============================="
-echo "Old directory remove finished"
-echo "============================="
+syncing = false
 
-# Init ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b vic-qpr1 --git-lfs
-echo "=========================="
-echo "ROM manifest init finished"
-echo "=========================="
+if [ "$syncing" = true ]; then
+	# Clear folders
+	cwd=$(pwd)
+	rm -rf .repo/local_manifests/
+	rm -rf device/oplus/denniz/
+	rm -rf device/oplus/mt6893-common/
+	rm -rf vendor/oplus/denniz/
+	rm -rf vendor/oplus/mt6893-common/
+	rm -rf vendor/evolution-priv/keys/
+	rm -rf kernel/oplus/mt6893/
+	rm -rf device/oplus/camera/
+	rm -rf hardware/oplus
+	rm -rf prebuilts/clang/host/linux-x86/
+	echo "============================="
+	echo "Old directory remove finished"
+	echo "============================="
 
-# Clone local manifest
-git clone https://github.com/liwhy1/local_manifests -b evox-10 .repo/local_manifests
-echo "============================="
-echo "Local manifest clone finished"
-echo "============================="
+	# Init ROM manifest
+	repo init -u https://github.com/Evolution-X/manifest -b vic-qpr1 --git-lfs
+	echo "=========================="
+	echo "ROM manifest init finished"
+	echo "=========================="
 
-# Clone signing keys
-git clone https://github.com/liwhy1/build_scripts -b evolution_keys vendor/evolution-priv/keys
-echo "==========================="
-echo "Signing keys clone finished"
-echo "==========================="
+	# Clone local manifest
+	git clone https://github.com/liwhy1/local_manifests -b evox-10 .repo/local_manifests
+	echo "============================="
+	echo "Local manifest clone finished"
+	echo "============================="
 
-# Sync
-/opt/crave/resync.sh
-echo "============="
-echo "Sync finished"
-echo "============="
+	# Clone signing keys
+	git clone https://github.com/liwhy1/build_scripts -b evolution_keys vendor/evolution-priv/keys
+	echo "==========================="
+	echo "Signing keys clone finished"
+	echo "==========================="
 
-# Clone WIP trees
-#rm -rf device/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip1 device/oplus/mt6893-common
-#rm -rf kernel/oplus/mt6893-common/
-#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
-#rm -rf vendor/oplus/denniz/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
-#rm -rf vendor/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
-#rm -rf device/oplus/camera/
-#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
-#rm -rf hardware/oplus/
-#git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
+	# Sync
+	/opt/crave/resync.sh
+	echo "============="
+	echo "Sync finished"
+	echo "============="
+else
+	echo "============="
+	echo "Skipping sync"
+	echo "============="
+fi
+
+wip = false
+
+if [ "$wip" = true ]; then
+	# Clone WIP trees
+	rm -rf device/oplus/mt6893-common/
+	git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip1 device/oplus/mt6893-common
+	rm -rf kernel/oplus/mt6893-common/
+	git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
+	rm -rf vendor/oplus/denniz/
+	git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
+	rm -rf vendor/oplus/mt6893-common/
+	git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
+	rm -rf device/oplus/camera/
+	git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
+	rm -rf hardware/oplus/
+	git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
+fi
 
 # Set up build environment
 cd $cwd
