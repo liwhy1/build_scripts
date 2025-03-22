d249bbdd414823aaf9f8c03f7f563022cad64bba
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-22 12:19:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 syncing=true
-wip=false
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
@@ -50,21 +49,19 @@ else
 fi
 
 
-if [ "$wip" = true ]; then
-	# Clone WIP trees
-	#rm -rf device/oplus/mt6893-common/
-	#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
-	#rm -rf kernel/oplus/mt6893-common/
-	#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
-	#rm -rf vendor/oplus/denniz/
-	#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
-	#rm -rf vendor/oplus/mt6893-common/
-	#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
-	#rm -rf device/oplus/camera/
-	#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
-	#rm -rf hardware/oplus/
-	#git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
-fi
+# Clone WIP trees
+#rm -rf device/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
+#rm -rf kernel/oplus/mt6893-common/
+#git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
+#rm -rf vendor/oplus/denniz/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
+#rm -rf vendor/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
+#rm -rf device/oplus/camera/
+#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
+#rm -rf hardware/oplus/
+#git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
 
 # Set up build environment
 cd $cwd
