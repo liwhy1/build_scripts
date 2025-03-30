1a78c9428b5453e0c8599a443b90f20545e4eb48
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-30 18:40:31 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -53,18 +53,18 @@ fi
 # Clone WIP trees
 rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip1 device/oplus/mt6893-common
-rm -rf hardware/mediatek/
-git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
+#rm -rf hardware/mediatek/
+#git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
 #rm -rf kernel/oplus/mt6893-common/
 #git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
 rm -rf vendor/oplus/mt6893-common/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1 vendor/oplus/mt6893-common
-rm -rf device/oplus/camera/
-git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
-rm -rf hardware/oplus/
-git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
+#rm -rf device/oplus/camera/
+#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1 device/oplus/camera
+#rm -rf hardware/oplus/
+#git clone https://github.com/liwhy1/hardware_oplus -b vic hardware/oplus
 
 # Set up build environment
 cd $cwd
