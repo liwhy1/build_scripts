41ebeef5b5afb4158cf22a13278f59a7a2764399
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-04-08 14:13:09 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -55,8 +55,8 @@ rm -rf device/oplus/mt6893-common/
 git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip1 device/oplus/mt6893-common
 #rm -rf device/oplus/denniz/
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
-rm -rf hardware/mediatek/
-git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
+#rm -rf hardware/mediatek/
+#git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
 #rm -rf kernel/oplus/mt6893-common/
 #git clone https://github.com/Kingslayer9988/android_kernel_oplus_mt6893 -b kingslayer kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
