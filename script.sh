1fe9dbbf98f23a1c13d1be8db77c7299d74efc7d
Liwhy <levi.toth2005@gmail.com>
2025-04-21 01:57:23 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -48,8 +48,8 @@ git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineag
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-22.1 device/oplus/denniz
 #rm -rf kernel/oplus/mt6893
 #git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
-#rm -rf vendor/oplus/denniz/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.1_wip vendor/oplus/denniz
+rm -rf vendor/oplus/denniz/
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.1_cam vendor/oplus/denniz
 #rm -rf vendor/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
 
