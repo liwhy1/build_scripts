fb013b3ebb8c1d337e23f8831eb293373195eca2
Liwhy <levi.toth2005@gmail.com>
2025-04-22 17:28:50 +0200

    Patch fsgen

--- script.sh
+++ script.sh
@@ -41,7 +41,6 @@ else
 	echo "============="
 fi
 
-
 # Clone WIP trees
 #rm -rf device/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1_wip1 device/oplus/mt6893-common
@@ -49,12 +48,18 @@ fi
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 #rm -rf hardware/mediatek/
 #git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
-rm -rf kernel/oplus/mt6893
-git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
+#rm -rf kernel/oplus/mt6893
+#git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
 #rm -rf vendor/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1 vendor/oplus/mt6893-common
+
+# Patch fsgen to fix conflicting lib issue
+sed -i '/soong_filesystem_creator {/,/}/s/enabled: true/enabled: false/' build/soong/fsgen/Android.bp
+echo "===================="
+echo "Fsgen patch finished"
+echo "===================="
 
 # Set up build environment
 cd $cwd
