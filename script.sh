1d188fea206f485047727bcb4c2c2985aa9330b4
Liwhy <levi.toth2005@gmail.com>
2025-04-23 15:39:26 +0200

    Clone original kernel
    "Some may wander on newer seas to discover something better, but when they realise the newer seas aren't ready to be seen, they might forget the reclone the older repo"
    -$wy

--- script.sh
+++ script.sh
@@ -48,8 +48,8 @@ fi
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 #rm -rf hardware/mediatek/
 #git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
-#rm -rf kernel/oplus/mt6893
-#git clone https://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
+rm -rf kernel/oplus/mt6893
+git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
 #rm -rf vendor/oplus/mt6893-common/
