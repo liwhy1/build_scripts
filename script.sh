d48f261731d394d6eb084058727fc7e2de935cb5
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-04-23 13:03:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-syncing=true
+syncing=false
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
@@ -48,8 +48,8 @@ fi
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 #rm -rf hardware/mediatek/
 #git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
-rm -rf kernel/oplus/mt6893
-git clone https://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
+#rm -rf kernel/oplus/mt6893
+#git clone https://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
 #rm -rf vendor/oplus/mt6893-common/
