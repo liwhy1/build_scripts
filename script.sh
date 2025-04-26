3c37fc59b2634df8f2558c5464e2501308a7cd1c
Liwhy <levi.toth2005@gmail.com>
2025-04-26 16:26:09 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -42,12 +42,12 @@ else
 fi
 
 # Clone WIP trees
-#rm -rf device/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
+rm -rf device/oplus/mt6893-common/
+git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
 #rm -rf device/oplus/denniz/
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 rm -rf hardware/mediatek/
-git clone https://github.com/mt6893-development/android_hardware_mediatek -b lineage-21 hardware/mediatek
+git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
 #rm -rf kernel/oplus/mt6893
 #git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
 #rm -rf vendor/oplus/denniz/
