2dcf731cf6b9226eb80da2435342f19cd55c0f17
Liwhy <levi.toth2005@gmail.com>
2025-05-23 16:07:03 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-syncing=false
+syncing=true
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
@@ -42,28 +42,28 @@ else
 fi
 
 # Clone WIP trees
-rm -rf device/oplus/mt6893-common/
-git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
+#rm -rf device/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
 #rm -rf device/oplus/denniz/
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
-rm -rf hardware/mediatek/
-git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
+#rm -rf hardware/mediatek/
+#git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
 #rm -rf kernel/oplus/mt6893
 #git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
-#rm -rf vendor/oplus/denniz/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-21_wip vendor/oplus/denniz
-#rm -rf vendor/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1 vendor/oplus/mt6893-common
+rm -rf vendor/oplus/denniz/
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.1_wip vendor/oplus/denniz
+rm -rf vendor/oplus/mt6893-common/
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
 
 # Patch fsgen to fix conflicting lib issue
-if awk '/soong_filesystem_creator {/,/}/ { if ($0 ~ /enabled:/) found=1 } END { exit !found }' build/soong/fsgen/Android.bp; then
-  sed -i '/soong_filesystem_creator {/,/}/s/enabled: true/enabled: false,/' build/soong/fsgen/Android.bp
-else
-  awk '/soong_filesystem_creator {/{in_block=1} in_block&&/enabled:/{found=1} /}/&&in_block{if(!found)print "    enabled: false,"; in_block=0} in_block&&/enabled: true/{sub(/enabled: true/,"enabled: false,")} {print}' build/soong/fsgen/Android.bp > temp && mv temp build/soong/fsgen/Android.bp
-fi
-echo "===================="
-echo "Fsgen patch finished"
-echo "===================="
+#if awk '/soong_filesystem_creator {/,/}/ { if ($0 ~ /enabled:/) found=1 } END { exit !found }' build/soong/fsgen/Android.bp; then
+#  sed -i '/soong_filesystem_creator {/,/}/s/enabled: true/enabled: false,/' build/soong/fsgen/Android.bp
+#else
+#  awk '/soong_filesystem_creator {/{in_block=1} in_block&&/enabled:/{found=1} /}/&&in_block{if(!found)print "    enabled: false,"; in_block=0} in_block&&/enabled: true/{sub(/enabled: true/,"enabled: false,")} {print}' build/soong/fsgen/Android.bp > temp && mv temp build/soong/fsgen/Android.bp
+#fi
+#echo "===================="
+#echo "Fsgen patch finished"
+#echo "===================="
 
 # Set up build environment
 cd $cwd
