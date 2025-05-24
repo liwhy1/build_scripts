e0ffc84d8c8487974e588455926a05e8feb7c622
Liwhy <levi.toth2005@gmail.com>
2025-05-24 16:25:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-syncing=true
+syncing=false
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
@@ -42,16 +42,16 @@ else
 fi
 
 # Clone WIP trees
-#rm -rf device/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
-#rm -rf device/oplus/denniz/
-#git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
+rm -rf device/oplus/mt6893-common/
+git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.1 device/oplus/mt6893-common
+rm -rf device/oplus/denniz/
+git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 #rm -rf hardware/mediatek/
 #git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
 #rm -rf kernel/oplus/mt6893
 #git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
 rm -rf vendor/oplus/denniz/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.1_wip vendor/oplus/denniz
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.1_cam vendor/oplus/denniz
 rm -rf vendor/oplus/mt6893-common/
 git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.1_wip vendor/oplus/mt6893-common
 
