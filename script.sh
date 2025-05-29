dcb545ba37ef32b3a1bc37146ffaba5945527d84
Liwhy <levi.toth2005@gmail.com>
2025-05-29 19:42:11 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-syncing=true
+syncing=false
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
@@ -48,8 +48,8 @@ fi
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.2_cam vendor/oplus/denniz
-#rm -rf vendor/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2_wip vendor/oplus/mt6893-common
+rm -rf vendor/oplus/mt6893-common/
+git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2 vendor/oplus/mt6893-common
 #rm -rf kernel/oplus/mt6893/
 #git clone http://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
 
@@ -58,14 +58,14 @@ cd $cwd
 . build/envsetup.sh
 
 # Patch kernel
-FILE="kernel/oplus/mt6893/KernelSU-Next/kernel/sucompat.c"
-grep -q '^extern bool ksu_su_compat_enabled;$' "$FILE"
-if [[ $? -eq 0 ]]; then
-  sed -i '/^extern bool ksu_su_compat_enabled;$/a bool ksu_devpts_hook = false;' "$FILE"
-  echo "Patch completed."
-else
-  echo "Patch failed."
-fi
+#FILE="kernel/oplus/mt6893/KernelSU-Next/kernel/sucompat.c"
+#grep -q '^extern bool ksu_su_compat_enabled;$' "$FILE"
+#if [[ $? -eq 0 ]]; then
+#  sed -i '/^extern bool ksu_su_compat_enabled;$/a bool ksu_devpts_hook = false;' "$FILE"
+#  echo "Patch completed."
+#else
+#  echo "Patch failed."
+#fi
 
 # Build signed
 cd $cwd
