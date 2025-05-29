52253d0eb2e9e708c09d9ccaa66193147ed8f784
Liwhy <levi.toth2005@gmail.com>
2025-05-29 18:04:30 +0200

    CLEANUP

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ if [ "$syncing" = true ]; then
 	rm -rf {device,vendor,kernel}/oplus
 	rm -rf vendor/*-priv/keys
 	echo "============================="
-	echo "Old directory remove finished"
+	echo "Old directory removal finished"
 	echo "============================="
 
 	# Init ROM manifest
@@ -42,22 +42,30 @@ else
 fi
 
 # Clone WIP trees
-rm -rf device/oplus/mt6893-common/
-git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.2 device/oplus/mt6893-common
-rm -rf device/oplus/denniz/
-git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
-#rm -rf hardware/mediatek/
-#git clone https://github.com/liwhy1/android_hardware_mediatek -b lineage-22.1 hardware/mediatek
-#rm -rf kernel/oplus/mt6893
-#git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
-rm -rf vendor/oplus/denniz/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.2_cam vendor/oplus/denniz
-rm -rf vendor/oplus/mt6893-common/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2_wip vendor/oplus/mt6893-common
+#rm -rf device/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.2 device/oplus/mt6893-common
+#rm -rf device/oplus/denniz/
+#git clone https://github.com/liwhy1/android_device_oplus_denniz -b evox-10 device/oplus/denniz
+#rm -rf vendor/oplus/denniz/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.2_cam vendor/oplus/denniz
+#rm -rf vendor/oplus/mt6893-common/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2_wip vendor/oplus/mt6893-common
+#rm -rf kernel/oplus/mt6893/
+#git clone http://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
 
 # Set up build environment
 cd $cwd
 . build/envsetup.sh
+
+# Patch kernel
+FILE="kernel/oplus/mt6893/KernelSU-Next/kernel/sucompat.c"
+grep -q '^extern bool ksu_su_compat_enabled;$' "$FILE"
+if [[ $? -eq 0 ]]; then
+  sed -i '/^extern bool ksu_su_compat_enabled;$/a bool ksu_devpts_hook = false;' "$FILE"
+  echo "Patch completed."
+else
+  echo "Patch failed."
+fi
 
 # Build signed
 cd $cwd
@@ -67,12 +75,3 @@ echo "=============="
 breakfast denniz userdebug
 make installclean
 m evolution
-
-echo "======================="
-echo "Building bootimage only"
-echo "======================="
-rm -rf kernel/oplus/mt6893/
-git clone http://github.com/dek0der/kernel_realme_RMX3031 -b KSU-Next-SUSFS kernel/oplus/mt6893
-. build/envsetup.sh
-breakfast denniz userdebug
-mka bootimage
