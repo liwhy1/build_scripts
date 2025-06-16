2dcfd7f15afd55d2587ea7e057faac6cecde9dc3
Liwhy <levi.toth2005@gmail.com>
2025-06-16 23:29:37 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -50,9 +50,8 @@ rm -rf vendor/oplus/denniz/
 git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.2 vendor/oplus/denniz
 rm -rf vendor/oplus/mt6893-common/
 git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2 vendor/oplus/mt6893-common
-rm -rf kernel/oplus/mt6893/
-git clone https://github.com/soulspark666/kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
-rm -rf device/oplus/camera
+#rm -rf kernel/oplus/mt6893/
+#git clone https://github.com/soulspark666/kernel_oplus_mt6893 -b kernelsu-next kernel/oplus/mt6893
 rm -rf vendor/oplus/camera
 git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera
 
