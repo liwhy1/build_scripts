22c717c0a0430595e6da57c4f54dad20bbba2069
liwhy1 <levi.toth2005@gmail.com>
2025-06-26 15:10:22 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -42,8 +42,8 @@ else
 fi
 
 # Clone WIP trees
-#rm -rf device/oplus/mt6893-common/
-#git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.2 device/oplus/mt6893-common
+rm -rf device/oplus/mt6893-common/
+git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.2_wip device/oplus/mt6893-common
 #rm -rf device/oplus/denniz/
 #git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-22.2 device/oplus/denniz
 #rm -rf vendor/oplus/denniz/
@@ -51,9 +51,9 @@ fi
 #rm -rf vendor/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2 vendor/oplus/mt6893-common
 rm -rf kernel/oplus/mt6893/
-git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b ksu_next_susfs kernel/oplus/mt6893
-#rm -rf vendor/oplus/camera
-#git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera
+git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-22.2 kernel/oplus/mt6893
+rm -rf vendor/oplus/camera
+git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera
 
 # Set up build environment
 cd $cwd
@@ -66,7 +66,7 @@ echo "Starting build"
 echo "=============="
 breakfast denniz userdebug
 make installclean
-mka bootimage
+mka bacon
 
 # Build non ksu boot image
 #cd $cwd
