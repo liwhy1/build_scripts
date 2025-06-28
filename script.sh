92bdbf2aa90f56a800f5e95e3be797441e385f13
liwhy1 <levi.toth2005@gmail.com>
2025-06-28 21:06:31 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -52,8 +52,8 @@ fi
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2 vendor/oplus/mt6893-common
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-23 kernel/oplus/mt6893 --depth=1
-#rm -rf device/oplus/camera/
-#git clone https://gitlab.com/liwhy1/android_device_oplus_camera -b lineage-22.1_wip device/oplus/camera
+rm -rf vendor/oplus/camera/
+#git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera
 
 
 # Set up build environment
