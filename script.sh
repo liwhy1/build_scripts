77f8a9b388cfb4f38d9b1cdf1498bce658552dd3
liwhy1 <levi.toth2005@gmail.com>
2025-07-09 12:18:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -44,16 +44,16 @@ fi
 # Clone WIP trees
 #rm -rf device/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineage-22.2_wip device/oplus/mt6893-common --depth 1
-rm -rf device/oplus/denniz/
-git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-22.2 device/oplus/denniz --depth 1
+#rm -rf device/oplus/denniz/
+#git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-22.2 device/oplus/denniz --depth 1
 #rm -rf vendor/oplus/denniz/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_denniz -b lineage-22.2 vendor/oplus/denniz --depth 1
 #rm -rf vendor/oplus/mt6893-common/
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2_wip vendor/oplus/mt6893-common --depth 1
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-22.2 kernel/oplus/mt6893 --depth 1
-#rm -rf vendor/oplus/camera
-#git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera --depth 1
+rm -rf vendor/oplus/camera
+git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.1_wip vendor/oplus/camera --depth 1
 
 # WPA3 fix
 #rm -rf external/wpa_supplicant_8
@@ -61,8 +61,6 @@ git clone https://github.com/liwhy1/android_device_oplus_denniz -b lineage-22.2 
 #cd external/wpa_supplicant_8
 #git fetch https://github.com/Adarsh0127-Elite/android_external_wpa_supplicant_8 252a7ddfdeab428bcb78c7f1dd170db814ee7687
 #git cherry-pick 252a7ddfdeab428bcb78c7f1dd170db814ee7687
-
-rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
 
 # Set up build environment
 cd $cwd
