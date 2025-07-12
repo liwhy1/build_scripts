41437ba78eb88ab1e182f23f5c69a4eda0d37af1
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-07-12 09:55:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -52,8 +52,8 @@ git clone https://github.com/liwhy1/android_device_oplus_mt6893-common -b lineag
 #git clone https://github.com/liwhy1/proprietary_vendor_oplus_mt6893-common -b lineage-22.2_wip vendor/oplus/mt6893-common
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-23 kernel/oplus/mt6893 --depth=1
-rm -rf vendor/oplus/camera/
-git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.2 vendor/oplus/camera --depth=1
+#rm -rf vendor/oplus/camera/
+#git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b lineage-22.2 vendor/oplus/camera --depth=1
 #rm -rf device/mediatek/sepolicy_vndr
 #git clone https://github.com/liwhy1/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr --depth=1
 
