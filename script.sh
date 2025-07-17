32d90b38d4e310baef0e12cf0426dca6ac894a3f
liwhy1 <levi.toth2005@gmail.com>
2025-07-17 15:50:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ if [ "$syncing" = true ]; then
 	echo "=========================="
 
 	# Clone local manifest
-	git clone https://github.com/liwhy1/local_manifests -b evox-10 .repo/local_manifests
+	git clone https://github.com/liwhy1/local_manifests -b evox-11 .repo/local_manifests
 	echo "============================="
 	echo "Local manifest clone finished"
 	echo "============================="
@@ -47,7 +47,7 @@ fi
 rm -rf device/oplus/MT6893/
 git clone https://github.com/liwhy1/android_device_oplus_MT6893 -b evox-11 device/oplus/MT6893 --depth=1
 #rm -rf vendor/oplus/MT6893/
-#git clone https://github.com/liwhy1/proprietary_vendor_oplus_MT6893 -b lineage-22.2 vendor/oplus/MT6893 --depth=1
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_MT6893 -b lineage-23 vendor/oplus/MT6893 --depth=1
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-22.2 kernel/oplus/mt6893 --depth=1
 #rm -rf vendor/oplus/camera/
