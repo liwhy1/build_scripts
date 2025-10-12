d2afd3e2ddb791dc028ddc481c40f0ddffc7ec81
liwhy1 <levi.toth2005@gmail.com>
2025-10-12 12:18:18 +0200

    bringup

--- script.sh
+++ script.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
-syncing=false
+syncing=true
 cwd=$(pwd)
 
 if [ "$syncing" = true ]; then
 	# Clear folders
+	echo "=========================="
+	echo "Removing old directories.."
+	echo "=========================="
 	rm -rf .repo/local_manifests
 	rm -rf {device,vendor,kernel}/oplus
 	rm -rf vendor/*-priv/keys
 	rm -rf prebuilts/*clang*
 	rm -rf external/*clang*
 	rm -rf external/wpa_supplicant_8
-	echo "============================="
-	echo "Old directory remove finished"
-	echo "============================="
+	rm -rf device/mediatek/sepolicy_vndr
 
 	# Init ROM manifest
+	echo "==========================="
+	echo "Initializing ROM manifest.."
+	echo "==========================="
 	repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
-	echo "=========================="
-	echo "ROM manifest init finished"
-	echo "=========================="
 
 	# Clone local manifest
-	git clone https://github.com/liwhy1/local_manifests -b lineage-23 .repo/local_manifests --depth 1
-	echo "============================="
-	echo "Local manifest clone finished"
-	echo "============================="
+	echo "========================"
+	echo "Cloning local manifest.."
+	echo "========================"
+	git clone https://github.com/liwhy1/local_manifests -b lineage-23 .repo/local_manifests --depth=1
 
 	# Clone signing keys
-	git clone https://github.com/liwhy1/build_scripts -b lineage_keys vendor/lineage-priv/keys --depth 1
-	echo "==========================="
-	echo "Signing keys clone finished"
-	echo "==========================="
+	echo "======================"
+	echo "Cloning signing keys.."
+	echo "======================"
+	git clone https://github.com/liwhy1/build_scripts -b lineage_keys vendor/lineage-priv/keys --depth=1
 
 	# Sync
+	echo "============================"
+	echo "Synchronizing repositories.."
+	echo "============================"
 	/opt/crave/resync.sh
-	echo "============="
-	echo "Sync finished"
-	echo "============="
+
 else
-	echo "============="
-	echo "Skipping sync"
-	echo "============="
+	echo "==============="
+	echo "Skipping sync.."
+	echo "==============="
 fi
 
 # Clone WIP trees
@@ -59,18 +61,18 @@ cd $cwd
 . build/envsetup.sh
 
 # Build signed
+echo "================"
+echo "Starting build.."
+echo "================"
 cd $cwd
-echo "=============="
-echo "Starting build"
-echo "=============="
 breakfast MT6893 userdebug
 make installclean
 mka bacon
 
 #cd $cwd
-#echo "======================="
-#echo "Building bootimage only"
-#echo "======================="
+#echo "========================="
+#echo "Building bootimage only.."
+#echo "========================="
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/liwhy1/android_kernel_oplus_mt6893 -b ksu-next-susfs kernel/oplus/mt6893
 #. build/envsetup.sh
