4860be4b025493c4356111af61dc2932f4bb8575
liwhy1 <levi.toth2005@gmail.com>
2025-10-15 21:38:34 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-syncing=true
+# Vars
+sync=true
+sign=false
+patch=false
 cwd=$(pwd)
 
-if [ "$syncing" = true ]; then
+if [ "$sync" = true ]; then
 	# Clear folders
 	echo "=========================="
 	echo "Removing old directories.."
@@ -29,10 +32,16 @@ if [ "$syncing" = true ]; then
 	git clone https://github.com/liwhy1/local_manifests -b lineage-23 .repo/local_manifests --depth=1
 
 	# Clone signing keys
-	echo "======================"
-	echo "Cloning signing keys.."
-	echo "======================"
-	git clone https://github.com/liwhy1/build_scripts -b lineage_keys vendor/lineage-priv/keys --depth=1
+	if [ "$sign" = true ]; then
+		echo "======================"
+		echo "Cloning signing keys.."
+		echo "======================"
+		git clone https://github.com/liwhy1/build_scripts -b lineage_keys vendor/lineage-priv/keys --depth=1
+	else
+		echo "=================="
+		echo "Skipping signing.."
+		echo "=================="
+	fi
 
 	# Sync
 	echo "============================"
@@ -40,6 +49,22 @@ if [ "$syncing" = true ]; then
 	echo "============================"
 	/opt/crave/resync.sh
 
+	# Apply patches
+	if [ "$patch" = true ]; then
+		echo "=================="
+		echo "Applying patches.."
+		echo "=================="
+		cd frameworks/base
+		git fetch https://github.com/Evolution-X/frameworks_base
+		git reset --hard FETCH_HEAD && git clean -fd
+		git fetch https://github.com/liwhy1/frameworks_base
+		git cherry-pick 2de218394de334414d37e6803cee26cba705d616
+		cd -
+	else
+		echo "=================="
+		echo "Skipping patches.."
+		echo "=================="
+	fi
 else
 	echo "==============="
 	echo "Skipping sync.."
