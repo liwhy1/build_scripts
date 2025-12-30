ba4874409760435f4d92dd1db1cd71449c4c7921
liwhy1 <levi.toth2005@gmail.com>
2025-12-30 15:16:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Vars
-sync=false
+sync=true
 sign=true
 cwd=$(pwd)
 
@@ -55,12 +55,12 @@ fi
 # Clone WIP trees
 rm -rf device/oplus/MT6893/
 git clone https://github.com/liwhy1/android_device_oplus_MT6893 -b evox-11 device/oplus/MT6893 --depth=1
-rm -rf vendor/oplus/MT6893/
-git clone https://github.com/liwhy1/proprietary_vendor_oplus_MT6893 -b lineage-23 vendor/oplus/MT6893 --depth=1
+#rm -rf vendor/oplus/MT6893/
+#git clone https://github.com/liwhy1/proprietary_vendor_oplus_MT6893 -b lineage-23 vendor/oplus/MT6893 --depth=1
 #rm -rf kernel/oplus/mt6893/
 #git clone https://github.com/mt6893-development/android_kernel_oplus_mt6893 -b lineage-22.2 kernel/oplus/mt6893 --depth=1
-rm -rf vendor/oplus/camera/
-git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b main vendor/oplus/camera --depth 1
+#rm -rf vendor/oplus/camera/
+#git clone https://gitlab.com/liwhy1/proprietary_vendor_oplus_camera -b main vendor/oplus/camera --depth 1
 
 # Set up build environment
 cd $cwd
